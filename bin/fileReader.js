8c436d743c70572d6e4797332966da0de57b897c
JackiieM <jackie.nct@gmail.com>
2022-09-27 17:12:02 -0300

    listo para la demo

--- bin/fileReader.js
+++ bin/fileReader.js
@@ -2,29 +2,34 @@
 // IMPORTS
 const path = require('path');
 const fs = require('fs')
-
+const urlRegEx = '^(?!mailto:)(?:(?:http|https|ftp)://)(?:\\S+(?::\\S*)?@)?(?:(?:(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[0-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})))|localhost)(?::\\d{2,5})?(?:(/|\\?|#)[^\\s]*)?$';
 // FUNCIONES DE LECTURA
-// Verifica si la ruta es un archivo, si es directory retorna false
-const routeType = (source) => {
-    if(source.isFile() === true) {
-        return true
-    } return false
-}
-// Transforma input links de relativos a absolutos
+// Transforma input de ruta de relativos a absolutos
 const getAbsoluteLink = (source) => {
     if(path.isAbsolute(source) === false){
         return path.resolve(source)
     } return source
 }
-// Busca y filtra archivos .md de una ruta
+// Verifica si la ruta es un archivo, si es un directorio retorna false
+const routeType = (source) => {
+    if(source.isFile() === true) {
+        return true
+    } return false
+}
+// Verifica si la extensión del archivo es .md
+const extValidator = (sourceExt, validExt) => {
+    if(sourceExt === validExt){
+        return true
+    }  return false
+}
+// Busca y filtra archivos .md de un directorio
 const filterFiles = (source, validExt) => {
     const foundFiles = [];
     fs.readdir(source,(err, files) => {
         if(err) {
-            console.log ("Esta ruta no es un directorio.")
+            console.log ("\x1b[31m","Esta ruta no es un directorio.", "\x1b[0m")
         } else {
-            
-            console.log ("Archivos encontrados:")
+            console.log ("\x1b[32m", "Archivos encontrados:", "\x1b[0m")
             files.forEach(file => {
                 if (path.extname(file) === validExt) {
                     foundFiles.push(file);
@@ -33,17 +38,11 @@ const filterFiles = (source, validExt) => {
             })
             if(foundFiles.length === 0) {
                 return console.log("No se encontraron archivos .md en este directorio.")
-            } return console.log(foundFiles)
+            } 
+            return foundFiles;
         }
     }) 
 }
-// Verifica si la extensión del archivo es .md
-const extValidator = (source, sourceExt, validExt) => {
-    if(source && sourceExt === validExt){
-        return "Archivo válido! continuando análisis..."
-    } else {
-        return "Whoops! Este no es un archivo .md, por favor ingresa un archivo compatible."
-    }
-}
+
 // break de lloración
 module.exports = {routeType, getAbsoluteLink, extValidator, filterFiles};
--- bin/index.js
+++ bin/index.js
@@ -11,7 +11,41 @@ const inputType = fs.statSync(absoluteLink) // Devuelve true si es archivo y fal
 const validExt = ".md" // Extensión deseada
 const fileExt = path.extname(absoluteLink) // Extensión del archivo
 
-
+// Lo que tengo para la demo :' D
+console.log("------------------------------------------")
+console.log("\x1b[32m", "Ruta absoluta:", "\x1b[0m", absoluteLink)
+console.log("------------------------------------------")
+console.log("¿Es un archivo?:", routeType(inputType))
+console.log("------------------------------------------")
+console.log("¿Es un archivo .md?:", extValidator(fileExt, validExt))
+console.log("------------------------------------------")
 filterFiles(absoluteLink, validExt)
+
+
+
+
+
+
+
+
+/*const mdLinks = (absoluteLink) => {
+    return new Promise((resolve, reject) => {
+        if(absoluteLink === undefined){
+            reject("Por favor ingresa una ruta para analizar :)")
+        } else {
+            if(routeType(absoluteLink) === true){
+                if(extValidator(fileExt, validExt) === true) {
+                    resolve(console.log("Archivo válido! continuando análisis..."))
+                } else {
+                    console.log("Whoops! Este no es un archivo .md, por favor ingresa un archivo compatible.")
+                }
+            } else {
+                console.log(filterFiles(absoluteLink, validExt))
+            }
+        }
+    })
+}
+
+module.exports = { mdLinks}*/
 
 
